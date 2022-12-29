6a9114e81e73365fd7a0e707b31ddeeabfb457bf
Eduard <ed.kech2005@mail.ru>
2022-12-29 09:59:33 -0500

    still keeping to the way bar+buttons

--- awesome/mk_touchpad.sh
+++ awesome/mk_touchpad.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# trackpad
 xinput set-prop 11 305 1
 xinput set-prop 11 284 1
 xinput set-prop 11 302 10
 xinput set-prop 11 293 0.02
+
+#mouse
+xinput set-prop 15 293 -0.35
--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
 MY AWESOME CONFIG
 
 TODO:
+ -2 BUG: when alt+t toggles app_bar on every window not just current workspace
+ -1 firefox
+  0 function keys
   1 app bar
   2 rofi
   3 bar
